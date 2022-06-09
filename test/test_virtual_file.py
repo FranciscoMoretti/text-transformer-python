bb8654d348c853be5e1e97d4a9374f6858194f61
Francisco Moretti <franciscoemoretti@gmail.com>
2022-06-09 09:53:04 +0100

    create tests cases for virtual_file

---
+++ test/test_virtual_file.py
@@ -0,0 +1,23 @@
+from virtual_file import VirtualFile, VirtualFileIO
+
+# content of test_tmp_path.py
+CONTENT = "content"
+
+
+def test_read_file(tmp_path):
+    path = _get_tmp_path_to_file(tmp_path)
+    path.write_text(CONTENT)
+    assert VirtualFileIO.read_from_path(path) == VirtualFile(lines=[CONTENT], path=path)
+
+
+def test_save_file(tmp_path):
+    path = _get_tmp_path_to_file(tmp_path)
+    virtual_file = VirtualFile(lines=[CONTENT], path=path)
+    VirtualFileIO.save_to_real_file(virtual_file)
+    assert path.read_text() == CONTENT
+
+
+def _get_tmp_path_to_file(tmp_path):
+    directory = tmp_path / "sub"
+    directory.mkdir()
+    return directory / "foo.txt"
