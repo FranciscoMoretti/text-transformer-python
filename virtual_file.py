3db31d0d1888f85265fde86a06b6e7edfae40752
Francisco Moretti <franciscoemoretti@gmail.com>
2022-06-09 09:17:35 +0100

    encoding specified

--- virtual_file.py
+++ virtual_file.py
@@ -12,10 +12,10 @@ class VirtualFile:
 class VirtualFileIO:
     @staticmethod
     def read_from_path(filepath: Path) -> VirtualFile:
-        with open(filepath) as open_file:
+        with open(filepath, encoding="UTF-8") as open_file:
             return VirtualFile(lines=open_file.readlines(), path=filepath)
 
     @staticmethod
     def save_to_real_file(virtual_file: VirtualFile):
-        with open(virtual_file.path, "w") as open_file:
+        with open(virtual_file.path, "w", encoding="UTF-8") as open_file:
             open_file.writelines(virtual_file.lines)
