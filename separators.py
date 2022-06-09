e3fe07b782a3bf6b0263b59a9bb12bfd2696b24d
Francisco Moretti <franciscoemoretti@gmail.com>
2022-06-09 10:21:31 +0100

    Implement conversions and tests for SeparatorList

--- separators.py
+++ separators.py
@@ -1,6 +1,6 @@
 from collections import UserList
 from dataclasses import dataclass
-from typing import List
+from typing import List, Dict
 
 
 @dataclass
@@ -12,3 +12,12 @@ class Separator:
 class SeparatorList(UserList):
     def get_values(self) -> List[str]:
         return [separator.value for separator in self.data]
+
+    @staticmethod
+    def from_list_of_dictionaries(list_of_dict: List[Dict[str, str]]):
+        return SeparatorList(
+            [
+                Separator(name=dictionary["name"], value=dictionary["value"])
+                for dictionary in list_of_dict
+            ]
+        )
--- test/test_separators.py
+++ test/test_separators.py
@@ -1,10 +1,30 @@
 from separators import Separator, SeparatorList
 
+SEPARATOR_LIST_SAMPLE = SeparatorList(
+    [
+        Separator(name="foo", value="foo_value"),
+        Separator(name="bar", value="bar_value"),
+    ]
+)
+
+SEPARATOR_LIST_DICT_SAMPLE = [
+    {
+        "name": "foo",
+        "value": "foo_value",
+    },
+    {
+        "name": "bar",
+        "value": "bar_value",
+    },
+]
+
 
 def test_get_values():
-    assert SeparatorList(
-        [
-            Separator(name="foo", value="foo_value"),
-            Separator(name="bar", value="bar_value"),
-        ]
-    ).get_values() == ["foo_value", "bar_value"]
+    assert SEPARATOR_LIST_SAMPLE.get_values() == ["foo_value", "bar_value"]
+
+
+def test_from_dict():
+
+    assert SEPARATOR_LIST_SAMPLE == SeparatorList.from_list_of_dictionaries(
+        SEPARATOR_LIST_DICT_SAMPLE
+    )
