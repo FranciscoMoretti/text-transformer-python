87711b68461f2a762520ad12c374beefb8e1a600
Francisco Moretti <franciscoemoretti@gmail.com>
2022-06-09 09:12:50 +0100

    format files

--- separators.py
+++ separators.py
@@ -1,15 +1,16 @@
 from dataclasses import dataclass
 from typing import List
 
+
 @dataclass
 class Separator:
     name: str
     value: str
+
 
 class SeparatorsRegistry:
     def __init__(self, delimiters: List[Separator]) -> None:
         self._separators: List[Separator] = delimiters
 
     def get_values(self) -> List[str]:
-         return [separator.value for separator in self._separators]
-
+        return [separator.value for separator in self._separators]
--- text_transform.py
+++ text_transform.py
@@ -1,7 +1,7 @@
 # Python program to
 # demonstrate with
 # statement
-  
+
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Dict, List
@@ -11,14 +11,11 @@ from virtual_file import VirtualFile, VirtualFileIO
 
 
 L = ["Geeks\n", "for\n", "Geeks\n"]
-  
-VirtualFileIO.save_to_real_file(virtual_file=
-    VirtualFile(
-        lines=L,
-        path=Path("myfile.txt")
-    )
+
+VirtualFileIO.save_to_real_file(
+    virtual_file=VirtualFile(lines=L, path=Path("myfile.txt"))
 )
-  
+
 # using readlines()
 count = 0
 print("Using readlines()")
@@ -27,26 +24,38 @@ lines: List[str] = []
 
 input_file = VirtualFileIO.read_from_path("CppCoreGuidelines.md")
 
+
 @dataclass
 class TextLine:
     text: str
     line_number: int
 
-def find_lines_that_cointains_strings(lines: List[str], match_patterns: List[str]) -> List[TextLine]:
-    lines_by_match_pattern: Dict[str, List[str]] = {match_pattern: [] for match_pattern in match_patterns}
-    for line_number, line_text  in enumerate(lines):
+
+def find_lines_that_cointains_strings(
+    lines: List[str], match_patterns: List[str]
+) -> List[TextLine]:
+    lines_by_match_pattern: Dict[str, List[str]] = {
+        match_pattern: [] for match_pattern in match_patterns
+    }
+    for line_number, line_text in enumerate(lines):
         for match_pattern in match_patterns:
             if match_pattern in line_text:
-                lines_by_match_pattern[match_pattern].append(TextLine(text=line_text, line_number=line_number))
+                lines_by_match_pattern[match_pattern].append(
+                    TextLine(text=line_text, line_number=line_number)
+                )
     return lines_by_match_pattern
 
 
-file_starter_separators = SeparatorsRegistry([
-    Separator(name='main', value='name="main"'),
-    Separator(name='abstract', value='name="S-abstract"'),
-    Separator(name='introduction', value='name="S-introduction"')
-])
+file_starter_separators = SeparatorsRegistry(
+    [
+        Separator(name="main", value='name="main"'),
+        Separator(name="abstract", value='name="S-abstract"'),
+        Separator(name="introduction", value='name="S-introduction"'),
+    ]
+)
 
-lines_of_separators = find_lines_that_cointains_strings(input_file.lines, file_starter_separators.get_values())
+lines_of_separators = find_lines_that_cointains_strings(
+    input_file.lines, file_starter_separators.get_values()
+)
 
 print(lines_of_separators)
