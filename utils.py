3275aca691a4f6f3f80ce601b5bd6502c2632851
Francisco Moretti <franciscoemoretti@gmail.com>
2022-06-14 12:03:13 +0100

    add pairwise to utils because it's from python 3.10

--- utils.py
+++ utils.py
@@ -1,5 +1,14 @@
+from itertools import tee
 from typing import Any, List
 
 
 def all_items_have_one_item_in_them(list_of_lists: List[List[Any]]) -> bool:
     return all(len(internal_list) == 1 for internal_list in list_of_lists)
+
+
+# From https://docs.python.org/3/library/itertools.html#itertools.pairwise
+def pairwise(iterable):
+    # pairwise('ABCDEFG') --> AB BC CD DE EF FG
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
