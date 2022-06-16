f38a7bbb62e096d5e7ccfd0fdc91747eda4d198d
Francisco Moretti <franciscoemoretti@gmail.com>
2022-06-16 09:45:39 +0100

    created unit tests for get_raw_lines_in_range

---
+++ test/test_virtual_file_processor.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from virtual_file import VirtualFile
+from virtual_file_processor import VirtualFileProcessor
+
+
+MULTILINE_CONTENT = ["Line 1\n", "Line 2\n", "Line 3\n", "Line 3\n"]
+VIRTUAL_FILE = VirtualFile(lines=MULTILINE_CONTENT, path=Path())
+
+
+def test_get_raw_lines_in_range():
+    file_processor = VirtualFileProcessor(VIRTUAL_FILE)
+    assert file_processor.get_raw_lines_in_range(end_line=2) == MULTILINE_CONTENT[:2]
+    assert file_processor.get_raw_lines_in_range(start_line=2) == MULTILINE_CONTENT[2:]
+    assert (
+        file_processor.get_raw_lines_in_range(start_line=1, end_line=2)
+        == MULTILINE_CONTENT[1:2]
+    )
