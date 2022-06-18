3169e5739c7e3565eda8b740af40b2000a2edc61
Francisco Moretti <franciscoemoretti@gmail.com>
2022-06-18 11:00:55 +0100

    update variable name

--- test/test_named_patterns.py
+++ test/test_named_patterns.py
@@ -6,18 +6,18 @@ PATTERNS = [
     {"value": "bar_value", "name": "bar"},
 ]
 
-SEPARATOR_LIST_SAMPLE = NamedPatternList(
+PATTERN_LIST_SAMPLE = NamedPatternList(
     [NamedPattern(name=pattern["name"], value=pattern["value"]) for pattern in PATTERNS]
 )
 
 
 def test_get_values():
-    assert SEPARATOR_LIST_SAMPLE.get_patterns() == [
+    assert PATTERN_LIST_SAMPLE.get_patterns() == [
         pattern["value"] for pattern in PATTERNS
     ]
 
 
 def test_get_regexps():
-    assert SEPARATOR_LIST_SAMPLE.get_regexps() == [
+    assert PATTERN_LIST_SAMPLE.get_regexps() == [
         re.compile(pattern["value"]) for pattern in PATTERNS
     ]
