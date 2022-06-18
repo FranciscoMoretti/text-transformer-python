9a6db187cd421eada1d85c66988d4b7bb6a2cd4f
Francisco Moretti <franciscoemoretti@gmail.com>
2022-06-18 11:34:14 +0100

    renamed class

---
+++ pattern_registry.py
@@ -0,0 +1,6 @@
+from named_patterns import NamedPatternList
+
+
+class PatternRegistry:
+    def __init__(self, patterns: NamedPatternList) -> None:
+        self.patterns = patterns
---
+++ test/test_patterns_registry.py
@@ -0,0 +1,16 @@
+from named_patterns import NamedPattern, NamedPatternList
+from pattern_registry import PatternRegistry
+
+PATTERNS = [
+    {"value": "foo_value", "name": "foo"},
+    {"value": "bar_value", "name": "bar"},
+]
+
+SEPARATOR_LIST_SAMPLE = NamedPatternList(
+    [NamedPattern(name=pattern["name"], value=pattern["value"]) for pattern in PATTERNS]
+)
+
+
+def test_patterns_registry():
+    patterns_registry = PatternRegistry(patterns=SEPARATOR_LIST_SAMPLE)
+    assert isinstance(patterns_registry, PatternRegistry)
