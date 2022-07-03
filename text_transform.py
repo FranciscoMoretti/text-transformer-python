78805ddb58a6976c17b390ad20fb00cda38a6ebd
Francisco Moretti <franciscoemoretti@gmail.com>
2022-07-03 11:46:59 +0100

    removed unnecessary code

--- text_transform.py
+++ text_transform.py
@@ -7,7 +7,6 @@ from src.matching import Matching
 from src.named_patterns import SearchConfiguration
 from src.pattern_registry import PatternRegistry
 from src.patterns_reader import PatternsReader
-from src.utils import all_items_have_one_item_in_them
 from src.virtual_file import VirtualFileIO
 from src.virtual_file_processor import VirtualFileProcessor
 
@@ -22,16 +21,6 @@ separator_registry = PatternRegistry(patterns=file_starter_separators)
 
 input_file = VirtualFileIO.read_from_path(INPUT_TEXT_FILE_PATH)
 file_processor = VirtualFileProcessor(input_file)
-lines_of_separators = file_processor.get_matched_lines_by_patterns(
-    separator_registry
-)
-
-if not all_items_have_one_item_in_them(
-    list_of_lists=list(lines_of_separators.values())
-):
-    print("All separators don't have one match in them")
-    print(lines_of_separators)
-    sys.exit()
 
 separated_files = file_processor.split_files_with_separators(
     separators=separator_registry
