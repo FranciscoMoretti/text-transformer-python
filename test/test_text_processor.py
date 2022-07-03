1c1c0d1b81650a462444e2b4dc3ad0d630a15a19
Francisco Moretti <franciscoemoretti@gmail.com>
2022-07-03 10:39:55 +0100

    parametrize unit test

--- test/test_text_processor.py
+++ test/test_text_processor.py
@@ -1,13 +1,23 @@
+import pytest
+
+from src.matching import SimpleMatching
 from src.named_patterns import SearchConfiguration
 from src.text_processor import TextProcessor
 
 MULTILINE_CONTENT = "Line 1\nLine 2\nLine 3\nLine 4\nLine 5\nLine 6\nLine 7\n"
 
 
-def test_search_matchings_of_pattern():
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        pytest.param(
+            SearchConfiguration(name="bar", regex_pattern="Line 3"),
+            [SimpleMatching(pattern_name="bar", start=14, end=16)],
+            marks=pytest.mark.xfail,
+        )
+    ],
+)
+def test_search_matchings_of_pattern(test_input, expected):
     file_processor = TextProcessor(MULTILINE_CONTENT)
-    pattern = SearchConfiguration(name="bar", regex_pattern="Line 3")
-
-    matchings = file_processor.search_matchings_of_pattern(pattern=pattern)
-    assert len(matchings) == 1
-    assert MULTILINE_CONTENT[matchings[0].start : matchings[0].end] == "Line 3"
+    matchings = file_processor.search_matchings_of_pattern(pattern=test_input)
+    assert matchings == expected
