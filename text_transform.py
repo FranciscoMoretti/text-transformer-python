de230549c4e0ae08a26ed73461e78c00ad81bb96
Francisco Moretti <franciscoemoretti@gmail.com>
2022-07-07 12:30:27 +0100

    adapted script to new usage

--- text_transform.py
+++ text_transform.py
@@ -3,48 +3,30 @@ import sys
 from pathlib import Path
 from typing import List, Optional
 
-from src.matching import Matching
+from src.file_processor import FileProcessor
+from src.matching import SimpleMatching
 from src.named_patterns import SearchConfiguration
-from src.pattern_registry import PatternRegistry
 from src.patterns_reader import PatternsReader
+from src.text_file import TextFile
+from src.text_file_io import TextFileIO
 from src.text_processor import TextProcessor
-from src.virtual_file import VirtualFileIO
-from src.virtual_file_processor import VirtualFileProcessor
-
-
-def flatten(list_of_lists):
-    return [item for sublist in list_of_lists for item in sublist]
-
 
 INPUT_TEXT_FILE_PATH = Path(".sandbox/CppCoreGuidelines.md")
 INPUT_SEPARATORS_FILE_PATH = Path(".sandbox/separators.json")
 OUTPUT_DIRECTORY_PATH = Path(".sandbox/output")
 
-file_starter_separators = PatternsReader.from_json_file(
+file_separators: List[SearchConfiguration] = PatternsReader.from_json_file(
     INPUT_SEPARATORS_FILE_PATH
 )
-separator_registry = PatternRegistry(patterns=file_starter_separators)
 
-input_file = VirtualFileIO.read_from_path(INPUT_TEXT_FILE_PATH)
-file_processor = VirtualFileProcessor(input_file)
+input_file = TextFileIO.read_from_path(INPUT_TEXT_FILE_PATH)
+input_file_processor = FileProcessor(input_file)
 
-separated_files = file_processor.split_files_with_separators(
-    separators=separator_registry
+separated_files: List[
+    TextFile
+] = input_file_processor.split_files_with_separators_starting_and_name(
+    separators=file_separators
 )
-
-# Experiment of testing the new api and draft for FileProcessor
-text_processor = TextProcessor(text="".join(input_file.lines))
-separators_matchings_lists = [
-    text_processor.find_matchings_of_pattern(separator)
-    for separator in file_starter_separators
-]
-separators_matchings = flatten(separators_matchings_lists)
-separators_matchings.sort(key=lambda x: x.start)
-separated_texts = text_processor.split_at_positions(
-    [matching.start for matching in separators_matchings]
-)
-
-filenames = [file.path.stem for file in separated_files]
 
 files_of_filenames = {file.path.name: file for file in separated_files}
 
@@ -52,8 +34,8 @@ name_tag_pattern = SearchConfiguration(
     name="name_tag", regex_pattern=r'.*name="(?P<tag>[A-Za-z0-9-]*)".*'
 )
 
-matchings_of_filenames = {
-    file.path.stem: VirtualFileProcessor(file).search_matchings_of_pattern(
+tag_matchings_of_filenames = {
+    file.path.stem: FileProcessor(file).search_matchings_of_pattern(
         name_tag_pattern
     )
     for file in separated_files
@@ -61,7 +43,7 @@ matchings_of_filenames = {
 
 
 def matching_list_to_value_of_match_group_list(
-    matching_list: List[Matching], match_group_name: str
+    matching_list: List[SimpleMatching], match_group_name: str
 ) -> List[Optional[str]]:
     return [
         matching_to_group_name_value(matching, match_group_name)
@@ -70,23 +52,25 @@ def matching_list_to_value_of_match_group_list(
 
 
 def matching_to_group_name_value(
-    matching: Matching, match_group_name: str
+    matching: SimpleMatching, match_group_name: str
 ) -> Optional[str]:
-    return matching.match.groupdict().get(match_group_name)
+    if match := name_tag_pattern.regex.match(matching.text):
+        return match.groupdict().get(match_group_name)
+    return None
 
 
 name_tags_of_filenames = {
     filename: matching_list_to_value_of_match_group_list(
         matching_list=matchings, match_group_name="tag"
     )
-    for filename, matchings in matchings_of_filenames.items()
+    for filename, matchings in tag_matchings_of_filenames.items()
 }
 
-tag_names_and_filenames = list(map(reversed, name_tags_of_filenames.items()))
+name_tags_and_filenames = list(map(reversed, name_tags_of_filenames.items()))
 
 filenames_of_tags = {}
 tag_set = set()
-for (tag_names, filename) in tag_names_and_filenames:
+for (tag_names, filename) in name_tags_and_filenames:
     for tag_name in tag_names:
         filenames_of_tags[tag_name] = filename
         if tag_name not in tag_set:
@@ -114,18 +98,24 @@ def replace_relative_tag_by_absolute_tag_in_match(
     return match_object.string.replace(relative_name, absolute_name)
 
 
+input_file_text_processor = TextProcessor(input_file.text)
+
 for relative_tag, absolute_tag in absolute_tag_of_relative_tag.items():
     pattern = SearchConfiguration(
         name="name_tag",
         regex_pattern=rf".*\[.*\]\((?P<relative>#{relative_tag})\).*",
     )
-    file_processor.substitute_pattern_with_replacement(
-        pattern=pattern,
-        replacement=replace_relative_tag_by_absolute_tag_in_match,
+    input_file_text_processor = TextProcessor(
+        input_file_text_processor.substitute_pattern_with_replacement(
+            pattern=pattern,
+            replacement=replace_relative_tag_by_absolute_tag_in_match,
+        )
     )
 
 separated_files_with_absolute_references = (
-    file_processor.split_files_with_separators(separators=separator_registry)
+    input_file_processor.split_files_with_separators_starting_and_name(
+        file_separators
+    )
 )
 
 for file in separated_files_with_absolute_references:
@@ -133,4 +123,4 @@ for file in separated_files_with_absolute_references:
         ".mdx"
     )
 for file in separated_files_with_absolute_references:
-    VirtualFileIO.save_to_real_file(virtual_file=file)
+    TextFileIO.save_to_real_file(file)
