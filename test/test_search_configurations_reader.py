78c039d0563483e16c9cee65998eadecd2defeda
Francisco Moretti <franciscoemoretti@gmail.com>
2022-07-07 20:00:30 +0100

    deleted other duplicated resource

--- test/test_search_configurations_reader.py
+++ test/test_search_configurations_reader.py
@@ -1,35 +1,24 @@
 import json
 from pathlib import Path
-from test.sample_resources import PATTERN_LIST_SAMPLE
+from test.sample_resources import PATTERN_LIST_SAMPLE, PATTERNS_SAMPLE
 from test.utils.tmp_path import _get_tmp_path_to_file
 
 from src.search_configuration import SearchConfiguration
 from src.search_configurations_reader import SearchConfigurationsReader
-
-PATTERN_LIST_DICT_SAMPLE = [
-    {
-        "name": "foo",
-        "regex_pattern": "foo_value",
-    },
-    {
-        "name": "bar",
-        "regex_pattern": "bar_value",
-    },
-]
 
 
 def test_from_list_of_dictionaries():
     assert (
         PATTERN_LIST_SAMPLE
         == SearchConfigurationsReader.from_list_of_dictionaries(
-            PATTERN_LIST_DICT_SAMPLE
+            PATTERNS_SAMPLE
         )
     )
 
 
 def test_from_json_file(tmp_path):
     filepath = _get_tmp_path_to_file(tmp_path)
-    filepath.write_text(json.dumps(PATTERN_LIST_DICT_SAMPLE))
+    filepath.write_text(json.dumps(PATTERNS_SAMPLE))
     assert PATTERN_LIST_SAMPLE == SearchConfigurationsReader.from_json_file(
         filepath
     )
