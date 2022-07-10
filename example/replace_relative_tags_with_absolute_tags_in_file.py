ae8ba4333f5cb2565f87e3426247a55c7a85aed4
Francisco Moretti <franciscoemoretti@gmail.com>
2022-07-10 18:32:10 +0100

    new variable names

--- example/replace_relative_tags_with_absolute_tags_in_file.py
+++ example/replace_relative_tags_with_absolute_tags_in_file.py
@@ -4,10 +4,10 @@ from src.text_processor import TextProcessor
 
 
 def replace_relative_tags_with_absolute_tags_in_file(
-    input_file, absolute_tag_of_relative_tag
+    file, absolute_tag_of_relative_tag
 ):
 
-    input_file_text_processor = TextProcessor(input_file.text)
+    text_processor = TextProcessor(file.text)
 
     for relative_tag, absolute_tag in absolute_tag_of_relative_tag.items():
         print(f"running for tag: {relative_tag} ")
@@ -16,10 +16,8 @@ def replace_relative_tags_with_absolute_tags_in_file(
             name="name_tag",
             regex_pattern=regex_pattern,
         )
-        input_file_text_processor.substitute_pattern_with_replacement(
+        text_processor.substitute_pattern_with_replacement(
             pattern=pattern, replacement=rf"[(\g<link>)]({absolute_tag})"
         )
 
-    return TextFile(
-        text=input_file_text_processor.get_text(), path=input_file.path
-    )
+    return TextFile(text=text_processor.get_text(), path=file.path)
